19bb6b64c7e02298a7d92798f967697a74745d12
Crowdbotics <engineering@crowdbotics.com>
2023-05-15 13:38:58 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "falling-bonus-dev-83426",
+  url: "https://falling-bonus-dev-83426.botics.co",
+  api: "https://falling-bonus-dev-83426.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
